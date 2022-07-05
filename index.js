68a3d94624bcdaa328aec6e107f305bceab71a3c
JacobTSkidmore <jacobtskidmore@icloud.com>
2022-07-04 23:20:09 -0400

    Add psuedocode

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('Hello, World!')
+// create computerPlay() and make it return random selection of rock-paper-scissors
+// create computerSelection variable and run computerPlay()
+// create playerSelection variable
+// ask rock-paper-scissors and store input in playerSelection (case-insensitive)
+// create singleRound() which plays playSelection against computerSelection
+    // rock beats scissors
+    // scissors beats paper
+    // paper beats rock
+    // equal ties
+    // return not log result
+// create game()
+    // create loop for 5 rounds of singleRound()
+    // create playerScore & computerScore, track singleRound(), log score variables
+    // create isWinner whoever has greater score
