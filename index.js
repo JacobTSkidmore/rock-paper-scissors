b3752010ce05f764bba4a489ce317c6ff91e72f5
JacobTSkidmore <jacobtskidmore@icloud.com>
2022-07-05 00:08:33 -0400

    Add player/computer selection prompt/computerPlay() respectively

--- index.js
+++ index.js
@@ -1,7 +1,30 @@
+// create random number function
+let randomNumber;
+function generateRandomNumber() {
+    return Math.floor(Math.random() * (9)) + 1;
+}
 // create computerPlay() and make it return random selection of rock-paper-scissors
+function computerPlay() {
+    randomNumber = generateRandomNumber();
+    if (randomNumber <= 3) {
+        return "rock";
+    }
+    else if (randomNumber <= 6) {
+        return "paper";
+    }
+    else {
+        return "scissors"
+    }
+}
+
+
 // create computerSelection variable and run computerPlay()
+let computerSelection;
+computerSelection = computerPlay();
 // create playerSelection variable
+let playerSelection;
 // ask rock-paper-scissors and store input in playerSelection (case-insensitive)
+playerSelection = prompt("Would you like to pick Rock, Paper or Scissors?");
 // create singleRound() which plays playSelection against computerSelection
     // rock beats scissors
     // scissors beats paper
