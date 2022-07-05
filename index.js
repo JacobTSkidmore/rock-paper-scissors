3361f55aa3b34d1c848e75073b0da6f20c70c9a4
JacobTSkidmore <jacobtskidmore@icloud.com>
2022-07-05 00:51:40 -0400

    Finish game | I need to commit more

--- index.js
+++ index.js
@@ -18,20 +18,97 @@ function computerPlay() {
 }
 
 
-// create computerSelection variable and run computerPlay()
+// create computerSelection variable
 let computerSelection;
-computerSelection = computerPlay();
 // create playerSelection variable
 let playerSelection;
-// ask rock-paper-scissors and store input in playerSelection (case-insensitive)
-playerSelection = prompt("Would you like to pick Rock, Paper or Scissors?");
+// create playerScore & computerScore
+let playerScore;
+let computerScore;
+// create victory() and defeat() to keep loops cleaner
+function victory() {
+    console.log(`You picked: ${playerSelection}. Your opponent picked: ${computerSelection}. \nYou won this round!`);
+    playerScore = ++playerScore;
+}
+function defeat() {
+    console.log(`You picked: ${playerSelection}. Your opponent picked: ${computerSelection}. \nYou lost this round!`);
+    computerScore = ++computerScore;
+}
 // create singleRound() which plays playSelection against computerSelection
-    // rock beats scissors
-    // scissors beats paper
-    // paper beats rock
-    // equal ties
-    // return not log result
+function singleRound() {
+    // ask rock-paper-scissors and store input in playerSelection (case-insensitive)
+    playerSelection = prompt("Would you like to pick Rock, Paper or Scissors?").toLowerCase();
+    // get new computerSelection
+    computerSelection = computerPlay();
+    // test for tie, loss, or win in all playerSelection scenarios
+    if (playerSelection === computerSelection) {
+        console.log("It was a tie, go again!")
+            singleRound();
+    }
+    else if (playerSelection === "rock") {
+        if (computerSelection === "paper") {
+            defeat();    
+        }
+        else {
+            victory();
+        }
+    }
+    else if (playerSelection === "paper") {
+        if (computerSelection === "scissors") {
+            defeat();    
+        }
+        else {
+            victory();
+        }
+    }
+    else if (playerSelection === "scissors") {
+        if (computerSelection === "rock") {
+            defeat();    
+        }
+        else {
+            victory();
+        }
+    }
+    else {
+        console.log("Please enter either rock, paper or scissors!")
+        singleRound();
+    }
+}
+
+// creat playAgain to allow repeated games
+let playAgain;
 // create game()
+function game() {
+    //reset score and winner when game is replayed
+    playerScore = 0;
+    computerScore = 0;
+    for (let i = 0; i <= 4; i++) {
+        singleRound();
+    }
+    console.log(`You won ${playerScore} rounds. Your opponent won ${computerScore} rounds.`);
+    if (playerScore > computerScore) {
+        console.log("You won the game!");
+    }
+    else {
+        console.log("You lost the game!")
+    }
+    function restart() {
+        playAgain = prompt("Would you like to play again?").toLowerCase();
+        if (playAgain === "yes") {
+            console.log("----------------------------\nNew game!")
+            game();
+        }
+        else if (playAgain === "no") {
+            console.log("Have a good day!");
+        }
+        else {
+            console.log("Please enter either yes or no!");
+            restart();
+        }
+    }
+    restart();
+    
+}
+game();
     // create loop for 5 rounds of singleRound()
-    // create playerScore & computerScore, track singleRound(), log score variables
-    // create isWinner whoever has greater score
+    
